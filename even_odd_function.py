9d084a5609753bde9f97a9a12d824ca73dac00d8
Sumant Kumar <sumant123@gmail.com>
2025-08-28 14:38:20 +0530

    1.0

--- even_odd_function.py
+++ even_odd_function.py
@@ -7,4 +7,17 @@ def check_even_odd(number):
 
 num = 7
 print(f"The number {num} is {check_even_odd(num)}.")
-# Output: The number 7 is Odd.
+
+# Function to check if a number is prime
+def is_prime(number):
+    if number <= 1:
+        return False
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:
+            return False
+    return True
+
+if is_prime(num):
+    print(f"The number {num} is Prime.")
+else:
+    print(f"The number {num} is Not Prime.")
